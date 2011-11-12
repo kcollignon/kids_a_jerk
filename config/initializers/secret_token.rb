aa0f5995c20966d248b868db7eb645ce81801bf8
Kevin Collignon <kevin@kevincollignon.com>
2011-11-11 23:20:46 -0700

    Initial Rails 3.0 deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KidsAJerk::Application.config.secret_token = '19e2733deb0bc1540c3d7cf823a7fb9a9acaad63ef92ae733d874763f979975668522953b53c71737c948e91e3290abb6831ea8af9884d9c47cc68fc61ff9d27'
